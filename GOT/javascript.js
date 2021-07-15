7f17b6a23dfeeedcbfe99f4e0fe5ef83f70d9ce4
Igor Kuruc <krc13@email.cz>
2021-07-15 22:28:44 +0200

    adjusting Clicking functionality to get corect coordinates

--- GOT/javascript.js
+++ GOT/javascript.js
@@ -31,3 +31,38 @@ function drawGrid() {
 
 rectangle("black", 0, 0, canvas.width, canvas.height);  
 drawGrid();
+
+
+function Clicking () {
+
+    // cursor coordinates
+    this.getCursorPosition = function  (canvas, event) {
+        const rect = canvas.getBoundingClientRect()
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+
+        //console.log("x: " + x + " y: " + y); //#! clean after
+
+        return [x,y]
+       // clickedSquare(x,y)
+    }
+
+    // coordinates of sqaure that is clicked into
+    this.clickedSquare = function (coordinates) {
+        row = Math.floor( coordinates[0] / tileSize );
+        column = Math.floor( coordinates[1]  / tileSize );
+       
+        // console.log(" column " + row + " row = " + column  );  //#! clean after
+        
+        return [row ,column]
+        //drawXO(row * tileSize,column* tileSize)
+    }
+}
+
+canvas.addEventListener('mousedown', function(e) {
+    let coordinatesClick = testClicking.getCursorPosition(canvas, e);
+    let coordinatesSquare = testClicking.clickedSquare(coordinatesClick);
+})
+
+
+let testClicking = new Clicking;
