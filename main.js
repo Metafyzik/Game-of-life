959990fea4254830a549aeb0e2b00c55b2ce4401
Igor Kuruc <krc13@email.cz>
2021-08-16 12:16:06 +0200

    drawing grid before showing modal

--- main.js
+++ main.js
@@ -176,24 +176,7 @@ const Game = {
     }
 }
 
-// Get the modal
-var modal = document.getElementById("myModal");
-// When the user clicks on <span> (x), close the modal
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-span.onclick = function() {
-  modal.style.display = "none";
-}
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-// canvas
+// CANVAS
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
@@ -210,7 +193,24 @@ drawGrid(colorCanvas);
 // array containing life cells
 let lifeCells = []; //#! Why not use object {x:number,y: number}, rename to liveCells
 
-// event listeners
+// MODAL
+var modal = document.getElementById("myModal");
+// When the user clicks on <span> (x), close the modal
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// EVENT LISTENERS
 canvas.addEventListener('mousedown',ClickCanvas);
 
 document.addEventListener("keydown", (e) => {
